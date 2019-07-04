be677dfa0f69c05705cc69bfaa98ac8ee4c51216
Malay Agarwal <malay.agarwal261016@outlook.com>
2019-07-04 15:15:28 +0530

    Add comments

--- driver.py
+++ driver.py
@@ -5,9 +5,12 @@ from dataload import *
 def main():
     X, y, theta, m, n = load_data("data/ex2data1.txt")
 
+    # Learning rate and number of iterations
     alpha, num_iters = 0.01, 1500
 
+    # Running gradient descent
     theta = gradient_descent(X, y, theta, m, alpha, num_iters)
+    # Getting model accuracy using accuracy() in regression.py
     print(f"Model accuracy: {accuracy(X, y, theta)}%")
 
 
